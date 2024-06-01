360529e0c58bee9b0730eaa8e88440ec01db1a12
Kevin-gram <k.nyiringan@alustudent.com>
2024-06-01 19:33:23 +0200

    guesing game

--- ES6_practical/script11.js
+++ ES6_practical/script11.js
@@ -12,3 +12,9 @@
 // n.firstName //Expected: John
 // n.lastName //Expected: Doe
 // n.name //Expected: John Doe
+function NameMe(first, last) {
+    this.firstName = first;
+    this.lastName = last;
+  this.name=`${this.firstName} ${this.lastName}`
+    return this.name;
+}
