7a1f3a12a7bd1b2e26917d03e1c63a5e3b5561e0
Kevin-gram <k.nyiringan@alustudent.com>
2024-07-09 11:59:06 +0200

    using generator function to get a multiplication table

---
+++ ES6_practical/typeScript/index6.ts
@@ -0,0 +1,12 @@
+// Generators can be used to wonderful things. You can use them for numerous things, but here is one specific example. You are studying for a test so you must practice your multiplication, but you don't have a multiplication table showing the different examples. You have decided to create a generator that prints out a limitless list of time tables.
+export function* generator(a: number): Generator<string> {
+  let i: number = 1;
+
+  while (true) {
+    let result: number = a * i;
+
+    yield `${a} x ${i} = ${result}`;
+
+    i += 1;
+  }
+}
