4eff9fec21621e6e2bd6d66346049266c783d3a9
Kevin-gram <k.nyiringan@alustudent.com>
2024-07-11 14:21:10 +0200

    checking the divisors of 3&5

--- ES6_practical/typeScript/index7.ts
+++ ES6_practical/typeScript/index7.ts
@@ -1,3 +1,4 @@
+// Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
 export function sumDigits(n: number): number {
   let res: number = 0;
 
