fcef3dfb264431f1c1f819d1980586607236beda
Kevin-gram <k.nyiringan@alustudent.com>
2024-07-29 17:06:17 +0200

    this function lets split a string with upper cases

--- ES6_practical/script32.js
+++ ES6_practical/script32.js
@@ -1,17 +1,15 @@
 function divisors(integer) {
-  
-    let res=  [];
-    
-    for(let i =0;i<integer;i++){
-      if(i!=1 && integer % i === 0){    
-        res.push(i)
-      }
+  let res = [];
+
+  for (let i = 0; i < integer; i++) {
+    if (i != 1 && integer % i === 0) {
+      res.push(i);
     }
-    
-    
-    if(res.length === 0)
-    {
-        return `${integer} is prime`
-    }
-    // this is  a return 
-    return res;
+  }
+
+  if (res.length === 0) {
+    return `${integer} is prime`;
+  }
+  // this is  a return
+  return res;
+}
