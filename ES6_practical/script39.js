747518bfa9fdf1fea9f29990a674b6543576bfa5
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-05 18:49:33 +0200

    checking winning ticket

---
+++ ES6_practical/script39.js
@@ -0,0 +1,22 @@
+// check if the winning ticket
+function bingo(ticket, win) {
+  let final = 0;
+
+  for (let subArray of ticket) {
+    let string = subArray[0];
+    let number = subArray[1];
+    let miniWin = false;
+
+    for (let char of string) {
+      if (char.charCodeAt(0) === number) {
+        miniWin = true;
+      }
+    }
+
+    if (miniWin) {
+      final += 1;
+    }
+  }
+
+  return final >= win ? "Winner!" : "Loser!";
+}
