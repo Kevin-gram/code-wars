89bfa0b7f24164273ed12e0071505158971ad376
Kevin-gram <k.nyiringan@alustudent.com>
2024-08-26 14:32:47 +0200

    removing unwanted comment

--- ES6_practical/script38.js
+++ ES6_practical/script38.js
@@ -2,7 +2,6 @@
 
 // Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
 
-// Write a function that accepts a sequence of unique integers between 0 and 9 (inclusive), and returns the missing element.
 function getMissingElement(asend) {
   asend.sort((a, b) => a - b);
 
