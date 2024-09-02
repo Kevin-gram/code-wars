3f73efa583ec90a47e5434b45468a9d0b4f36129
Kevin-gram <k.nyiringan@alustudent.com>
2024-09-02 23:11:11 +0200

    changing some comments

--- ES6_practical/script38.js
+++ ES6_practical/script38.js
@@ -1,7 +1,5 @@
 // Fellow code warrior, we need your help! We seem to have lost one of our sequence elements, and we need your help to retrieve it!
 
-// Our sequence given was supposed to contain all of the integers from 0 to 9 (in no particular order), but one of them seems to be missing.
-
 function getMissingElement(asend) {
   asend.sort((a, b) => a - b);
 
