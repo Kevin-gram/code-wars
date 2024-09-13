7027f72b44f3e68a270392cef5d3500433c7bfee
Kevin-gram <k.nyiringan@alustudent.com>
2024-09-13 10:55:27 +0200

    refactor

--- ES6_practical/script34.js
+++ ES6_practical/script34.js
@@ -1,20 +1,8 @@
-//Write Number in Expanded Form
-// You will be given a number and you will need to return it as a string in Expanded Form. For example:
-
-// expandedForm(12); // Should return '10 + 2'
-// expandedForm(42); // Should return '40 + 2'
-// expandedForm(70304); // Should return '70000 + 300 + 4'
-// NOTE: All numbers will be whole numbers greater than 0.
-
-function expandedForm(num) {
-  let stringNum = String(num).split("");
-  let reverse = stringNum.reverse().join("");
-  let numWithZeroes = [];
-  for (let i = 0; i < reverse.length; i++) {
-    numWithZeroes.push(reverse[i] + "0".repeat(i));
+class Eve extends Adam {
+  constructor(name) {
+    super(name, "Female");
   }
-  return numWithZeroes
-    .filter((item) => Number(item[0] != "0"))
-    .reverse()
-    .join(" + ");
 }
+
+const eve = new Eve("Eve");
+eve.speak();
